a4b5efcaf0c89cd1c98b1e47a8856655838ae688
Blueflowers <Canniverrumble@protonmail.com>
2022-09-10 09:06:32 +0200

    player exit now saves chunks around the player

--- scenes/main.lua
+++ scenes/main.lua
@@ -16,6 +16,9 @@ skyValue1 = {(width/50),(height/50)}
 skyValue2 = {(width/66.6),(height/66.6)}
 print(inspect(skyValue2))
 function s.quit()
+  for i,chunk in pairs(global.multiverse[global.currentUniverse].chunks) do
+    worldFunctions.saveChunk(global.multiverse[global.currentUniverse],chunk)
+  end
   global.playerData.position = global.multiverse[global.currentUniverse].actors[global.currentActor].position:array()
   if exists(love.filesystem.getSaveDirectory().."/".."playerData.json") then
     love.filesystem.write("playerData.json",lunajson.encode(global.playerData))
